c6e1952cd8c4f52da03ed21a72eb8eb370865f8e
Garbakbl <121131132+Garbakbl@users.noreply.github.com>
2023-04-13 15:28:41 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,11 @@
-from scipy.stats import norm
+from statsmodels.stats.proportion import proportions_ztest
 
 chat_id = 356550601
 
-def solution(x_success: int, x_cnt: int, y_success: int, y_cnt: int) -> bool:
-    p1 = x_success / x_cnt
-    p2 = y_success / y_cnt
-    p = (x_success + y_success) / (x_cnt + y_cnt)
-    z_alpha = norm.ppf(1 - 0.04/2)
-    z = (p1 - p2) / (p * (1 - p) * (1/x_cnt + 1/y_cnt))**0.5
-    return abs(z) > z_alpha
+def solution(x_success: int, 
+             x_cnt: int, 
+             y_success: int, 
+             y_cnt: int) -> bool:
+    alpha = 0.04
+    _, p_value = proportions_ztest([x_success, y_success], [x_cnt, y_cnt], alternative='two-sided')
+    return p_value < alpha
