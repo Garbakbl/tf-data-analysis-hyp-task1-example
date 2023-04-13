0388c47a2b1df247092be1a1b2344e35167a4b71
Garbakbl <121131132+Garbakbl@users.noreply.github.com>
2023-04-13 17:08:15 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,6 +1,6 @@
-from statsmodels.stats.proportion import proportions_ztest
 import pandas as pd
 import numpy as np
+import scipy.stats as st
 
 chat_id = 356550601
 
@@ -8,6 +8,15 @@ def solution(x_success: int,
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
-    alpha = 0.04
-    _, p_value = proportions_ztest([x_success, y_success], [x_cnt, y_cnt], alternative='two-sided')
-    return p_value < alpha
+    n1 = x_cnt
+    n2 = y_cnt
+    X1 = x_success
+    X2 = y_success
+    p1 = X1 / n1
+    p2 = X2 / n2
+    p_combined = (X1 + X2) / (n1 + n2)
+    E1 = n1 * p_combined
+    E2 = n2 * p_combined
+    chi_squared_stat = ((X1 - E1)**2 / E1) + ((X2 - E2)**2 / E2)
+    p_value = 1 - st.chi2.cdf(chi_squared_stat, 1)
+    return p_value < 0.04
